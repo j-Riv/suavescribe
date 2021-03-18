3cd16bff835f3a106a4e8f7b7b754676876d0e7e
j-Riv <jriv@suavecito.com>
2021-03-17 18:32:17 -0700

    Created Edit-Subscriptions, still need to link queries / mutations to buttons. Will possibly merge Subscription & Edit Subscription Page.

--- src/pages/edit-subscription.tsx
+++ src/pages/edit-subscription.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useCallback } from 'react';
 import { useRouter } from 'next/router';
 import {
   Button,
@@ -17,7 +17,7 @@ import {
   TextStyle,
 } from '@shopify/polaris';
 import { TitleBar } from '@shopify/app-bridge-react';
-import { useQuery, useMutation } from '@apollo/client';
+import { useQuery, useLazyQuery, useMutation } from '@apollo/client';
 import { Redirect } from '@shopify/app-bridge/actions';
 import styled from 'styled-components';
 import {
@@ -30,27 +30,40 @@ import {
 import { formatDate, formatId } from '../utils/formatters';
 
 function EditSubscription() {
-  const [nextBillingDate, setNextBillingDate] = useState<string>();
-  const [lineItems, setLineItems] = useState<any[]>();
-  const [lineItem, setLineItem] = useState<string>();
-  const [lineItemQuantity, setLineItemQuantity] = useState<string>();
   const router = useRouter();
+  // const { loading, error, data } = useQuery(GET_SUBSCRIPTION_BY_ID, {
+  //   variables: {
+  //     id: router.query.id,
+  //   },
+  //   onCompleted: data => setData(data),
+  // });
   const { loading, error, data } = useQuery(GET_SUBSCRIPTION_BY_ID, {
     variables: {
       id: router.query.id,
     },
-    onCompleted: data => {
-      const d = data.subscriptionContract;
-      console.log('Setting Next Billing Date', d.nextBillingDate.split('T')[0]);
-      setNextBillingDate(d.nextBillingDate.split('T')[0]);
-      console.log('Setting Line Item', d.lines.edges[0].node.productId);
-      setLineItem(d.lines.edges[0].node.productId);
-      console.log('Setting Line Item Quantity', d.lines.edges[0].node.quantity);
-      setLineItemQuantity(d.lines.edges[0].node.quantity);
-      console.log('Setting Line Items', d.lines.edges);
-      setLineItems(d.lines.edges);
-    },
+    onCompleted: data => setInitialData(data),
   });
+
+  const [nextBillingDate, setNextBillingDate] = useState<string>();
+  const [lineItems, setLineItems] = useState<any[]>();
+  const [lineItem, setLineItem] = useState<string>();
+  const [lineItemQuantity, setLineItemQuantity] = useState<string>();
+  const [paymentMethod, setPaymentMethod] = useState<string>();
+
+  const setInitialData = (data: any) => {
+    console.log('data', data);
+    const d = data.subscriptionContract;
+    console.log('Setting Next Billing Date', d.nextBillingDate.split('T')[0]);
+    setNextBillingDate(d.nextBillingDate.split('T')[0]);
+    console.log('Setting Line Item', d.lines.edges[0].node.productId);
+    setLineItem(d.lines.edges[0].node.productId);
+    console.log('Setting Line Item Quantity', d.lines.edges[0].node.quantity);
+    setLineItemQuantity(String(d.lines.edges[0].node.quantity));
+    console.log('Setting Line Items', d.lines.edges);
+    setLineItems(d.lines.edges);
+    console.log('Setting Payment Method Id', d.customerPaymentMethod.id);
+    setPaymentMethod(d.customerPaymentMethod.id);
+  };
 
   const handleSubmitNextBillingDate = () => {
     console.log('submission', nextBillingDate);
@@ -96,71 +109,88 @@ function EditSubscription() {
           Edit Subscription ({formatId(data.subscriptionContract.id)})
         </TextStyle>
       </Heading>
-      {data && (
-        <Layout>
-          <Layout.AnnotatedSection
-            title="Next Billing Date"
-            description="Change / Update Next Billing Date"
-          >
-            <Card sectioned>
-              <Form onSubmit={() => handleSubmitNextBillingDate}>
-                <FormLayout>
-                  <TextField
-                    value={nextBillingDate}
-                    onChange={nextBillingDate =>
-                      handleNextBillingDateChange(nextBillingDate)
-                    }
-                    label="Next Billing Date"
-                    type="date"
-                  />
-                  <Stack distribution="trailing">
-                    <Button primary submit>
-                      Save
-                    </Button>
-                  </Stack>
-                </FormLayout>
-              </Form>
-            </Card>
-          </Layout.AnnotatedSection>
-          <Layout.AnnotatedSection
-            title="Product Updates"
-            description="Select Product to Update Quantity"
-          >
-            <Card sectioned>
-              <Form onSubmit={() => handleSubmitLineItemQuantity}>
-                <FormLayout>
-                  <Select
-                    label="Line Items"
-                    options={data.subscriptionContract.lines.edges.map(line => {
-                      return {
-                        label: `${line.node.title} - ${line.node.variantTitle}`,
-                        value: line.node.productId,
-                      };
-                    })}
-                    onChange={lineItem => handleLineItemChange(lineItem)}
-                    value={lineItem}
-                  />
-                  <Select
-                    label="Quantity"
-                    options={['1', '2', '3', '4', '5'].map(value => {
-                      return { label: value, value: value };
-                    })}
-                    onChange={lineItemQuantity =>
-                      handleLineItemQuantityChange(lineItemQuantity)
-                    }
-                    value={lineItemQuantity}
-                  />
-                  <Stack distribution="trailing">
-                    <Button primary submit>
-                      Save
-                    </Button>
-                  </Stack>
-                </FormLayout>
-              </Form>
-            </Card>
-          </Layout.AnnotatedSection>
-        </Layout>
-      )}
+      <Layout>
+        <Layout.AnnotatedSection
+          title="Next Billing Date"
+          description="Change / Update Next Billing Date"
+        >
+          <Card sectioned>
+            <Form onSubmit={() => handleSubmitNextBillingDate}>
+              <FormLayout>
+                <TextField
+                  value={nextBillingDate}
+                  onChange={nextBillingDate =>
+                    handleNextBillingDateChange(nextBillingDate)
+                  }
+                  label="Next Billing Date"
+                  type="date"
+                />
+                <Stack distribution="trailing">
+                  <Button primary submit>
+                    Update
+                  </Button>
+                </Stack>
+              </FormLayout>
+            </Form>
+          </Card>
+        </Layout.AnnotatedSection>
+        <Layout.AnnotatedSection
+          title="Product"
+          description="Select Product to Update Quantity"
+        >
+          <Card sectioned>
+            <Form onSubmit={() => handleSubmitLineItemQuantity}>
+              <FormLayout>
+                <Select
+                  label="Item"
+                  options={data.subscriptionContract.lines.edges.map(line => {
+                    return {
+                      label: `${line.node.title} - ${line.node.variantTitle}`,
+                      value: line.node.productId,
+                    };
+                  })}
+                  onChange={lineItem => handleLineItemChange(lineItem)}
+                  value={lineItem}
+                />
+                <TextField
+                  value={lineItemQuantity}
+                  onChange={lineItemQuantity =>
+                    handleLineItemQuantityChange(lineItemQuantity)
+                  }
+                  label="Quantity"
+                  type="number"
+                />
+                <Stack distribution="trailing">
+                  <Button primary submit>
+                    Update
+                  </Button>
+                </Stack>
+              </FormLayout>
+            </Form>
+          </Card>
+        </Layout.AnnotatedSection>
+        <Layout.AnnotatedSection
+          title="Payment Method"
+          description="Send Update Payment Method Email"
+        >
+          <Card sectioned>
+            <Form onSubmit={() => console.log('clicked')}>
+              <FormLayout>
+                <TextField
+                  label="Payment Method ID"
+                  disabled
+                  value={paymentMethod}
+                />
+                <Stack distribution="trailing">
+                  <Button primary submit>
+                    Update
+                  </Button>
+                </Stack>
+              </FormLayout>
+            </Form>
+          </Card>
+        </Layout.AnnotatedSection>
+      </Layout>
     </Page>
   );
 }
