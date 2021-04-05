d5a8410721461f9e1340b456eefd3357a0f80f87
j-Riv <jriv@suavecito.com>
2021-04-05 12:27:55 -0700

    Dockerized App

--- app/src/handlers/index.ts
+++ app/src/handlers/index.ts
@@ -5,11 +5,11 @@ import { GET_SUBSCRIPTION_BY_ID } from './queries/get-subscription-contract-by-i
 import { GET_SELLING_PLAN_GROUP_BY_ID } from './queries/get-selling-plan-group-by-id';
 import { DELETE_SELLING_PLAN_GROUP } from './mutations/delete-selling-plan-group';
 import { UPDATE_PAYMENT_METHOD } from './mutations/update-payment-method';
+import { UPDATE_SELLING_PLAN_GROUP } from './mutations/update-selling-plan-group';
 import { UPDATE_SUBSCRIPTION_CONTRACT } from './mutations/update-subscription-contract';
 import { UPDATE_SUBSCRIPTION_DRAFT } from './mutations/update-subscription-draft';
 import { COMMIT_SUBSCRIPTION_DRAFT } from './mutations/commit-subscription-draft';
 import { UPDATE_SUBSCRIPTION_DRAFT_LINE } from './mutations/update-subscription-draft-line';
-import { UPDATE_SELLING_PLAN_GROUP } from './mutations/update-selling-plan-group';
 
 export {
   GET_SUBSCRIPTION_CONTRACTS,
@@ -19,9 +19,9 @@ export {
   GET_SELLING_PLAN_GROUP_BY_ID,
   DELETE_SELLING_PLAN_GROUP,
   UPDATE_PAYMENT_METHOD,
+  UPDATE_SELLING_PLAN_GROUP,
   UPDATE_SUBSCRIPTION_CONTRACT,
   UPDATE_SUBSCRIPTION_DRAFT,
   COMMIT_SUBSCRIPTION_DRAFT,
   UPDATE_SUBSCRIPTION_DRAFT_LINE,
-  UPDATE_SELLING_PLAN_GROUP,
 };
