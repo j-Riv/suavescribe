f544290c4288398cd080807c92d5ad0ef970d9ff
j-Riv <jriv@suavecito.com>
2021-07-08 16:08:02 -0700

    Updated Generate Next Billing Date --> added day, year. Changed month to 30 days instead of just incrementing month due to minconsistency in days

--- app/server/utils/index.ts
+++ app/server/utils/index.ts
@@ -1,13 +1,24 @@
 export const generateNextBillingDate = (
-  interval: 'MONTH' | 'WEEK',
+  interval: 'DAY' | 'WEEK' | 'MONTH' | 'YEAR',
   intervalCount: number
 ) => {
   let now = new Date();
-  if (interval === 'MONTH') {
-    now.setMonth(now.getMonth() + intervalCount);
-  } else {
-    now.setDate(now.getDate() + intervalCount * 7);
+
+  switch (interval) {
+    case 'DAY':
+      now.setDate(now.getDate() + intervalCount);
+      break;
+    case 'WEEK':
+      now.setDate(now.getDate() + intervalCount * 7);
+      break;
+    case 'MONTH':
+      // now.setMonth(now.getMonth() + intervalCount);
+      now.setDate(now.getDate() + intervalCount * 30);
+      break;
+    case 'YEAR':
+      now.setDate(now.getDate() + intervalCount * 365);
   }
+
   return new Date(now).toISOString().substring(0, 10);
   // return new Date(now).toISOString().substring(0, 10) + 'T00:00:00Z';
 };
